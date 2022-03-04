cffd28317e5d6f1466559aa2b096d56deac567e5
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-04 15:49:52 -0500

    Video 3 del curso de reacts con HolaMundo

---
+++ main.js
@@ -0,0 +1,18 @@
+//console.log("Hola mundo")
+//let
+//const
+
+let variable = 0
+console.log(variable)
+
+variable = "Nuevo valor de variable"
+
+console.log(variable)
+
+const variable2 = 0
+
+console.log(variable2)
+
+variable2 = 12
+
+console.log(variable2)
