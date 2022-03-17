67598cd87131bcaa321543310ec1fcaa95abe099
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-17 16:02:29 -0500

    Primer app con reactjs video 11

---
+++ Proyects/hello-world/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        h1 >
+        Bienvenido a React <
+        /h1> <
+        p className = "App-intro" > Hola mundo < /p> < /
+        header >
+        <
+        /div>
+    );
+}
+
+export default App;
