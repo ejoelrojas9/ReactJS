22619a3dcf499f7b63335b8bae5ae4ac9e268003
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-17 17:06:02 -0500

    Configuración css de header y configuración de la clase App

--- Proyects/hello-world/src/App.js
+++ Proyects/hello-world/src/App.js
@@ -1,24 +1,26 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        h1 >
-        Bienvenido a React <
-        /h1> <
-        p className = "App-intro" > Hola mundo < /p> < /
-        header >
-        <
-        /div>
-    );
+class App extends Component {
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            header className = "App-header" >
+            <
+            img src = { logo }
+            className = "App-logo"
+            alt = "logo" / >
+            <
+            h1 className = 'App-title' > Bienvenido a React < /h1>  < /
+            header > <
+            p className = "App-intro" >
+            Hola mundo <
+            /p> < /
+            div >
+        );
+    }
 }
 
 export default App;
