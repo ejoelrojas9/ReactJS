777e0e56fa649b9c146af75bcc18da2dbbd24c42
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-20 00:47:05 -0500

    Se elimino la propiedad css del logo pointer-events: none;, y se activo la interacción del estilo para el proyecto en algunos elementos video 14

--- Proyects/hello-world/src/App.js
+++ Proyects/hello-world/src/App.js
@@ -3,12 +3,21 @@ import Cabecera from './components/Cabecera'
 import './App.css';
 
 class App extends Component {
+    manejaClick = texto => {
+        console.log(texto)
+    }
     render() {
+        const miau = 'Bienvenido miau'
+        const guau = 'Bienvenido guau'
         return ( <
             div className = "App" >
             <
-            Cabecera / >
-            <
+            Cabecera miau = { miau }
+            manejaClick = { this.manejaClick }
+            />  <
+            Cabecera miau = { guau }
+            manejaClick = { this.manejaClick }
+            /><
             p className = "App-intro" >
             Hola mundo <
             /p> < /
--- Proyects/hello-world/src/components/Cabecera.jsx
+++ Proyects/hello-world/src/components/Cabecera.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import logo from '../logo.svg';
 
-
 export default class Cabecera extends Component {
+    manejaClick = () => {
+        const { manejaClick, miau } = this.props
+        manejaClick(miau)
+    }
     render() {
+        const { manejaClick, miau } = this.props
         return ( <
             header className = "App-header" >
             <
-            img src = { logo }
+            img onClick = { manejaClick }
+            onClick = { this.manejaClick }
+            src = { logo }
+
             className = "App-logo"
             alt = "logo" / >
             <
-            h1 className = 'App-title' > Bienvenido a React < /h1>   < /
+            h1 onClick = { manejaClick }
+            className = "App-title" > { miau } < /h1>   < /
             header >
         )
     }
