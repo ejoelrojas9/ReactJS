ce8eb2e12e603d5157861733f5a72e9183a9cf63
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-21 16:42:35 -0500

    Se utilizo el manejo de cambio onChange y se utilizo el guardado de datos en el metodo state para luego verificar como se hace el envio de datos en un formulario en react video 21

--- Proyects/hello-world/src/App.js
+++ Proyects/hello-world/src/App.js
@@ -1,33 +1,15 @@
-import React, { Component } from 'react';
-import Cabecera from './components/Cabecera'
-import P from './components/P';
-
-import './App.css';
+import React, { Component } from 'react'
+import FormularioSimple from './components/FormularioSimple'
 
 class App extends Component {
-    state = {
-        miau: 'Bienvenido a miau'
-    }
-    cambiarTextoDelEstado = () => {
-        this.setState({ miau: 'Hola mundo' })
-        console.log('Voy Bien')
-    }
-    manejaClick = texto => {
-        console.log(texto)
-    }
     render() {
-        const { miau } = this.state
-        const texto = 'Bienvenido a miau'
         return ( <
             div className = "App" >
             <
-            Cabecera miau = { texto }
-            manejaClick = { this.manejaClick }
-            /> <
-            p onClick = { this.cambiarTextoDelEstado } > { miau } <
-            /p> < /
-            div >
-        );
+            FormularioSimple / >
+            <
+            /div>
+        )
     }
 }
 
