33bf1088d7218626f57aff40352fab2e2ce78be9
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-22 10:30:46 -0500

    Configuración de GET y POST con Fetch para hacer consultas ajax a un servidor, en este caso jsonplacerholder video 24

--- Proyects/hello-world/src/App.js
+++ Proyects/hello-world/src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 import FormularioSimple from './components/FormularioSimple'
 
 class App extends Component {
+    constructor() {
+        super()
+            /*GET
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(x => x.json())
+            .then(x => console.log(x))*/
+            // POST
+        fetch('https://jsonplaceholder.typicode.com/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Emilce Peña',
+                    username: 'Mami',
+                })
+            }).then(x => x.json())
+            .then(x => console.log(x))
+
+    }
     render() {
         return ( <
             div className = "App" >
